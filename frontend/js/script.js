4a8c826a683dceac5eecb3625d9af4465d715b4b
txb22 <tranxuanbacit@gmail.com>
2021-09-28 21:41:07 +0700

    first commit

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,4 +1,8 @@
-var swiper = new Swiper(".mySwiper", {});
+var swiper = new Swiper(".mySwiper", {
+      autoplay: {
+            delay: 3000
+      }
+});
 var swiper = new Swiper(".mySlide", {
       slidesPerView: 3,
       spaceBetween: 30,
