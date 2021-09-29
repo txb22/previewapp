45227ad29e7c69821b4fba8eedf8385e7b7fb755
txb22 <tranxuanbacit@gmail.com>
2021-09-29 15:53:30 +0700

    first commit

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,8 +1,16 @@
 var swiper = new Swiper(".mySwiper", {
       autoplay: {
-            delay: 3000
+            delay: 5000
       }
 });
+$(".mySwiper").hover(function () {
+      (this).swiper.autoplay.stop();
+}, function () {
+      (this).swiper.autoplay.start();
+});
+
+
+
 var swiper = new Swiper(".mySlide", {
       slidesPerView: 3,
       spaceBetween: 30,
@@ -12,4 +20,55 @@ var swiper = new Swiper(".mySlide", {
       }, autoplay: {
             delay: 3000,
       }
+})
+      ;
+$(".mySlide").hover(function () {
+      (this).swiper.autoplay.stop();
+      console.log('stop')
+}, function () {
+      (this).swiper.autoplay.start();
 });
+const MENU_MOBILE = {
+      btnMenu: document.querySelector(".btn__menu"),
+      menu: document.querySelector(".nav-menu"),
+      body: document.querySelector("body"),
+      slideOpen: false,
+      initHeight: 0,
+      self: this,
+      init: function () {
+            this.toggleEvent();
+            // this.addPlus();
+            // this.showChild();
+      },
+      toggleEvent: function () {
+            MENU_MOBILE.btnMenu.addEventListener("click", function () {
+                  var _this = this.classList.toggle("active");
+                  if (MENU_MOBILE.slideOpen) {
+                        MENU_MOBILE.slideOpen = false;
+                        MENU_MOBILE.menu.style.display = "none";
+                  } else {
+                        MENU_MOBILE.slideOpen = true;
+                        MENU_MOBILE.menu.style.display = "block";
+                  }
+            });
+      },
+      // addPlus: function () {
+      //       var hasMenu = MENU_MOBILE.menu.querySelectorAll("ul > li > ul");
+      //       for (var i = 0; i < hasMenu.length; i++) {
+      //             var btn = document.createElement("i");
+      //             btn.className = 'fa fa-angle-right';
+      //             hasMenu[i].parentElement.appendChild(btn);
+      //       }
+      // },
+      // showChild: function () {
+      //       var btnShowChild = MENU_MOBILE.menu.querySelectorAll("i");
+      //       // btnShowChild.addEventListener("click",function(){
+      //       //     var _this = this;
+      //       //     var child =  _this.parentElement.children("ul");
+      //       //     if(typeof(child) != undefined){
+      //       //         child.classList.toggle("active");
+      //       //     }
+      //       // });
+      // }
+}
+MENU_MOBILE.init();
